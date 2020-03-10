8f9ec80464caf3dfbc0fc75142f4b825e4c5ac1b
João Victor Marks <jovic.marks@gmail.com>
2020-03-10 19:41:23 -0300

    finalversion

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: process.env.REACT_APP_API_URL,
+   baseURL: 'http://192.168.56.1:1119' ,
 });
 
 //http://192.168.56.1:1119
+
+// .env REACT_APP_API_URL=http://localhost:1119
 
 export default api;
