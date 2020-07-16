b63ceb8054f4488ab742d57c4ff5e490d89b0f98
JVMarks <jovic.marks@gmail.com>
2020-07-16 13:54:42 -0300

    v2.0

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,40 +1,42 @@
 import React, { useState } from 'react';
-import api from '../../services/api';
+import api from '../../services/api.js';
 
 export default function Login({ history }) {
- const [email, setEmail] = useState('');
+  const [email, setEmail] = useState('')
 
   async function handleSubmit(event) {
-     event.preventDefault();
- 
-    const response = await api.post('/sessions', { email });
-   
- 
-   const { _id } = response.data;
- 
-    localStorage.setItem('user', _id);
+    event.preventDefault()
 
-    history.push('./dashboard');
-   }
- 
+    const response = await api.post('/sessions', {
+      email: email
+    })
+
+    const { _id } = response.data
+
+    localStorage.setItem('user', _id)
+
+    history.push('/dashboard')
+  }
+
+
   return (
     <>
-    <p>
-    Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-  </p>
+      <p>
+        Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+            </p>
 
-  <form onSubmit={handleSubmit}>
-    <label htmlFor="email">E-MAIL *</label>
-    <input 
-    id="email" 
-    type="email" 
-    placeholder="Seu melhor e-mail"
-    value= {email}
-    onChange={event => setEmail(event.target.value)}
-    />
-    
-       <button className="btn" type="submit">Entrar</button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-MAIL *</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Seu melhor e-mail"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+
+        <button className="btn" type="submit">Entrar</button>
+      </form>
     </>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter , Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
@@ -8,11 +8,11 @@ import New from './pages/New';
 export default function Routes() {
   return (
     <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component={Login}/>
-      <Route path="/dashboard" component={Dashboard}/>
-      <Route path="/new" component={New}/>
-    </Switch>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/new" component={New} />
+      </Switch>
     </BrowserRouter>
-  );
+  )
 }
